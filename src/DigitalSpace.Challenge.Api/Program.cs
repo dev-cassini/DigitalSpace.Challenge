7fc2ff9868a711d28e5f3da7b45f453aae2a3824
Ryan Dremza <ryan.dremza@gmail.com>
2023-07-13 02:04:36 +0100

    Add hosted service to startup

--- src/DigitalSpace.Challenge.Api/Program.cs
+++ src/DigitalSpace.Challenge.Api/Program.cs
@@ -1,9 +1,11 @@
+using DigitalSpace.Challenge.Api.BackgroundServices;
 using DigitalSpace.Challenge.Infrastructure;
 using DigitalSpace.Challenge.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddHostedService<PeriodicRandomVehicleBackgroundService>();
 
 var app = builder.Build();
 
