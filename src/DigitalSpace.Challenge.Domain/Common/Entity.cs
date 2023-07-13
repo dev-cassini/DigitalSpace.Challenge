c76263c87fc5dbd83da0eee4796dff3535b0900a
Ryan Dremza <ryan.dremza@gmail.com>
2023-07-13 01:50:23 +0100

    Domain - add domain events to entity

--- src/DigitalSpace.Challenge.Domain/Common/Entity.cs
+++ src/DigitalSpace.Challenge.Domain/Common/Entity.cs
@@ -1,3 +1,19 @@
+using MediatR;
+
 namespace DigitalSpace.Challenge.Domain.Common;
 
-public abstract class Entity { }
+public abstract class Entity
+{
+    private readonly List<INotification> _domainEvents = new();
+    public IReadOnlyCollection<INotification> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(INotification eventItem)
+    {
+        _domainEvents.Add(eventItem);
+    }
+
+    public void RemoveDomainEvent(INotification @event)
+    {
+        _domainEvents.Remove(@event);
+    }
+}
